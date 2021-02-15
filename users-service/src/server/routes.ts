7c6f1e9529325128888cc8f4be4746991e469b1d
Bruno Ochotorena <hackerhilt@gmail.com>
2021-02-15 00:12:15 -0300

    Adicionando suporte para criar um usuário

--- users-service/src/server/routes.ts
+++ users-service/src/server/routes.ts
@@ -1,11 +1,13 @@
 import config from "config";
 import { Express } from "express";
 import { getConnection, getRepository } from "typeorm";
+import omit from "lodash.omit";
 import User from "#root/db/entities/User";
 import UserSession from "#root/db/entities/UserSession";
 import generateUUID from "#root/helpers/generateUUID";
 import passwordCompareSync from "#root/helpers/passwordCompareSync";
 import dayjs from "dayjs";
+import hashPassword from "#root/helpers/hashPassword";
 
 const USER_SESSION_EXPIRY_HOURS = <number>(
   config.get("USER_SESSION_EXPIRY_HOURS")
@@ -14,10 +16,11 @@ const USER_SESSION_EXPIRY_HOURS = <number>(
 const setupRoutes = (app: Express) => {
   const connection = getConnection();
   const userRepository = getRepository(User);
+  const userSessionRepository = getRepository(UserSession);
 
   app.post("/sessions", async (req, res, next) => {
     if (!req.body.username || !req.body.password) {
-      return next(Error("Invalid body!"));
+      return next(new Error("Invalid body!"));
     }
 
     try {
@@ -25,7 +28,9 @@ const setupRoutes = (app: Express) => {
         {
           username: req.body.username,
         },
-        { select: ["id", "passwordHash"] }
+        {
+          select: ["id", "passwordHash"],
+        }
       );
 
       if (!user) return next(new Error("Invalid username!"));
@@ -50,9 +55,65 @@ const setupRoutes = (app: Express) => {
         .createQueryBuilder()
         .insert()
         .into(UserSession)
-        .values([userSession]);
+        .values([userSession])
+        .execute();
 
       return res.json(userSession);
+    } catch (err) {
+      return next(err);
+    }
+  });
+
+  app.delete("/sessions/:sessionId", async (req, res, next) => {
+    try {
+      const userSession = await userSessionRepository.findOne(
+        req.params.sessionId
+      );
+
+      if (!userSession) return next(new Error("Invalid session ID"));
+
+      await userSessionRepository.remove(userSession);
+
+      return res.end();
+    } catch (err) {
+      return next(err);
+    }
+  });
+
+  app.get("/sessions/:sessionId", async (req, res, next) => {
+    try {
+      const userSession = await userSessionRepository.findOne(
+        req.params.sessionId
+      );
+
+      if (!userSession) return next(new Error("Invalid session ID"));
+
+      return res.json(userSession);
+    } catch (err) {
+      return next(err);
+    }
+  });
+
+  app.post("/users", async (req, res, next) => {
+    if (!req.body.username || !req.body.password) {
+      return next(new Error("Invalid body!"));
+    }
+
+    try {
+      const newUser = {
+        id: generateUUID(),
+        passwordHash: hashPassword(req.body.password),
+        username: req.body.username,
+      };
+
+      await connection
+        .createQueryBuilder()
+        .insert()
+        .into(User)
+        .values([newUser])
+        .execute();
+
+      return res.json(omit(newUser, ["passwordHash"]));
     } catch (err) {
       next(err);
     }
