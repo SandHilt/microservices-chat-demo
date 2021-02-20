5a5f0c7f081a08c2966e076d477429e5954b8c93
Bruno Ochotorena <hackerhilt@gmail.com>
2021-02-20 12:29:58 -0300

    Adicionando resolver para User

--- api-gateway/src/adapters/UsersService.ts
+++ api-gateway/src/adapters/UsersService.ts
@@ -3,11 +3,40 @@ import got from "got";
 
 const USERS_SERVICE_URI = <string>config.get("USERS_SERVICE_URI");
 
+export interface User {
+  id: string;
+  username: string;
+  createdAt: string;
+}
+
+export interface UserSession {
+  createdAt: string;
+  expiresAt: string;
+  id: string;
+  userId: string;
+}
+
 export default class UsersService {
-  static async fetchUserSession({ sessionId }: { sessionId: string }) {
+  static async fetchUser({ userId }: { userId: string }): Promise<User | null> {
+    const body = await got
+      .get(`${USERS_SERVICE_URI}/users/${userId}`)
+      .json<User | null>();
+
+    if (!body) return null;
+
+    return body;
+  }
+
+  static async fetchUserSession({
+    sessionId,
+  }: {
+    sessionId: string;
+  }): Promise<UserSession | null> {
     const body = await got
       .get(`${USERS_SERVICE_URI}/sessions/${sessionId}`)
-      .json();
+      .json<UserSession | null>();
+
+    if (!body) return null;
 
     return body;
   }
