984f224c6245fd97f89ed04cf999b4a59a455f30
Bruno Ochotorena <hackerhilt@gmail.com>
2021-06-08 00:50:12 -0300

    Adicionando o cliente com graphql

--- chat-app/src/components/Root.tsx
+++ chat-app/src/components/Root.tsx
@@ -1,13 +1,49 @@
-import React from "react";
-import { Button, Intent } from "@blueprintjs/core";
+import { gql } from "@apollo/client";
+import { useRecoilState } from "recoil";
+import React, { useEffect, useState } from "react";
+import { Spinner } from "@blueprintjs/core";
+import styled from "styled-components";
+import apolloClient from "#root/api/apolloClient";
+import userSessionAtom from "#root/recoil/atoms/userSession";
 
-interface RootProps {}
+const SpinnerWrapper = styled.main`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`;
 
-function Root({}: RootProps): JSX.Element {
-  return (
-    <main>
-      <Button intent={Intent.SUCCESS}>Click Me</Button>
-    </main>
+const query = gql`
+  query {
+    userSession(me: true) {
+      user {
+        username
+      }
+    }
+  }
+`;
+
+function Root() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [userSession, setUserSession] = useRecoilState(userSessionAtom);
+
+  const apolloRequest = async () => {
+    const res = await apolloClient.query({ query });
+    const userSession = res.data?.userSession ?? null;
+    setUserSession(userSession);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    apolloRequest();
+  }, []);
+
+  return isLoading ? (
+    <SpinnerWrapper>
+      <Spinner />
+    </SpinnerWrapper>
+  ) : (
+    <pre>{JSON.stringify(userSession, null, 2)}</pre>
   );
 }
 
--- chat-app/src/index.tsx
+++ chat-app/src/index.tsx
@@ -1,10 +1,20 @@
+import { ApolloProvider } from "@apollo/client";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "normalize.css";
 
 import React from "react";
 import { render } from "react-dom";
+import { RecoilRoot } from "recoil";
 
-import Root from "./components";
+import apolloClient from "#root/api/apolloClient";
+import Root from "#root/components";
 
-render(<Root />, document.getElementById("app"));
+render(
+  <ApolloProvider client={apolloClient}>
+    <RecoilRoot>
+      <Root />
+    </RecoilRoot>
+  </ApolloProvider>,
+  document.getElementById("app")
+);
